463a3e957d82fb07e960f905c2ef8966bf9f3af4
Pankaj Sharma <Pankaj Sharma@LAPTOP-GE4N93UN>
2016-07-20 18:49:07 +1200

    DataFirst CRUD MVC

---
+++ DatabaseFirst/Controllers/Student2Controller.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DatabaseFirst.Models;
+using System.Net;
+
+namespace DatabaseFirst.Controllers
+{
+    public class Student2Controller : Controller
+    {
+        // GET: Student2
+        public ActionResult Index()
+        {
+            using (var db = new CrudEntities())
+                return View(db.Students.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ViewModel.Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new CrudEntities())
+                {
+
+                    var st = new Student()
+                    {
+                        FirstMiddleName = student.FirstName,
+                        LastName = student.LastName,
+                        EnrollmentDate = DateTime.Now,
+                    };
+                    db.Students.Add(st);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Students");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var db = new CrudEntities())
+            {
+                DatabaseFirst.Models.Student student = db.Students.Find(id);
+                if (student == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(student);
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var db = new CrudEntities())
+            {
+                DatabaseFirst.Models.Student student = db.Students.Find(id);
+                if (student == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(student);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new CrudEntities())
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(student);
+         }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var db = new CrudEntities())
+            {
+                DatabaseFirst.Models.Student student = db.Students.Find(id);
+                if (student == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(student);
+            }
+         }
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            using (var db = new CrudEntities())
+            {
+                DatabaseFirst.Models.Student student = db.Students.Find(id);
+                db.Students.Remove(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+                
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                using (var db = new CrudEntities())
+                { db.Dispose(); }
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DatabaseFirst/ViewModel/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseFirst.ViewModel
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+    }
+}
